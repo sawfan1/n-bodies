3393afaa0dbeda1e1be629ed37bb6f3bfef2ea4f
sawfan1 <sawfannn1@gmail.com>
2026-09-22 16:22:16 +0600

    Trying to render fonts

--- main.py
+++ main.py
@@ -1,26 +1,37 @@
 import pygame
+import pygame.freetype
+
+pygame.init()
 
 # parameters
 FPS = 60
-WIDTH = 700
-HEIGHT = 400
+WIDTH = 800
+HEIGHT = 600
+GAME_FONT = pygame.freetype.Font("Aloevera.ttf", 24)
 
 # boilerplate
-pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 
 class Panel:
-  pos = [10, 10]
-  dimensions = [300, 200]
+  margin = 20
+  dimensions = [200, 100]
+  pos = [WIDTH-dimensions[0]-margin, margin]
   background = (255, 255, 255)
+  text = "Panel"
   rect_obj = pygame.Rect(pos[0], pos[1], dimensions[0], dimensions[1])
+
+  def render(self):
+    pygame.draw.rect(screen, self.panel.background, self.panel.rect_obj, 2)
+    GAME_FONT.render_to(screen, (40, 350), self.text, (255, 255 ,240))
+
 
 class Body:
   pass
 
 class Game:
   running = True
+  panel = Panel()
   def handle_input(self):
     # quit game response
     for event in pygame.event.get():
@@ -31,10 +42,8 @@ class Game:
     # background
     screen.fill("black")
 
-    # panel
-    panel = Panel()
-    pygame.draw.rect(screen, panel.background, panel.rect_obj, 2)
-
+    self.panel.render()
+    
     # ignore boilerplate
     pygame.display.flip()
     clock.tick()
