963a3071a4a6d1c52896e188f216cac5a01f1fcc
sawfan1 <sawfannn1@gmail.com>
2026-09-22 18:32:17 +0600

    Added some basic UI

--- main.py
+++ main.py
@@ -7,23 +7,49 @@ pygame.init()
 FPS = 60
 WIDTH = 800
 HEIGHT = 600
-GAME_FONT = pygame.freetype.Font("Aloevera.ttf", 24)
+GAME_FONT = pygame.freetype.Font("Aloevera.ttf", 20)
+SMALL_FONT = pygame.freetype.Font("Aloevera.ttf", 12)
+STARTING_MASS = 5
 
 # boilerplate
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 
+# extraneous variables
+WHITE = (255, 255, 255)
+
 class Panel:
   margin = 20
-  dimensions = [200, 100]
+  dimensions = [200, 120]
   pos = [WIDTH-dimensions[0]-margin, margin]
-  background = (255, 255, 255)
-  text = "Panel"
+  background = WHITE
+  text_color = WHITE
+  text = "Body mass: (KG)"
   rect_obj = pygame.Rect(pos[0], pos[1], dimensions[0], dimensions[1])
 
+  # input rectangle settings
+  ipos = [pos[0] + 15, pos[1] + 45]
+  idimensions = [dimensions[0]-30, 30]
+  input_obj = pygame.Rect(ipos[0], ipos[1], idimensions[0], idimensions[1])
+  text_active = False
+
+  # submit button settings
+  bd = [60, 25]
+  bpos = [pos[0] + dimensions[0]/2 - 30, ipos[1]+40]
+  b_object = pygame.Rect(bpos[0], bpos[1], bd[0], bd[1])
+
   def render(self):
-    pygame.draw.rect(screen, self.panel.background, self.panel.rect_obj, 2)
-    GAME_FONT.render_to(screen, (40, 350), self.text, (255, 255 ,240))
+    pygame.draw.rect(screen, self.background, self.rect_obj, 2)
+    GAME_FONT.render_to(screen, (self.pos[0] + 15, self.pos[1] + 15), self.text, WHITE)
+
+    if self.text_active:
+      bg = (0, 0, 255)
+    else:
+      bg = WHITE
+
+    pygame.draw.rect(screen, bg, self.input_obj, 2)
+    pygame.draw.rect(screen, WHITE, self.b_object, 2)
+    SMALL_FONT.render_to(screen, (self.bpos[0]+15, self.bpos[1]+10), "SET", WHITE)
 
 
 class Body:
@@ -36,7 +62,14 @@ class Game:
     # quit game response
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
-        self.running = False    
+        self.running = False
+
+      if event.type == pygame.MOUSEBUTTONDOWN:
+        if self.panel.input_obj.collidepoint(event.pos):
+          print("clicked me ")
+          self.panel.text_active = True
+        else:
+          self.panel.text_active = False
 
   def draw(self):
     # background
